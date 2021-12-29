e3cf15af78bbda4d22c98a5bfb65ce57176d41b5
dominick <dominickugas@gmail.com>
2021-12-29 17:17:05 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :category_items
+  resources :categories
+  resources :items
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/migrate/20211228201641_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.boolean :sold, default: false
+      t.integer :rating
+      t.string :image
+      t.references :seller, foreign_key: { to_table: 'users' }
+      t.references :buyer, foreign_key: { to_table: 'users' }
+      
+      t.timestamps
+    end
+  end
+end
