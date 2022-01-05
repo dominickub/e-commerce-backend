95c71be7c3a29ce3d090e75ccce0b635156e1aac
dominick <dominickugas@gmail.com>
2022-01-05 18:15:06 -0500

    added image in the seed data and added css to card component

--- db/schema.rb
+++ db/schema.rb
@@ -15,27 +15,12 @@ ActiveRecord::Schema.define(version: 2021_12_30_190607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "category_items", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_category_items_on_category_id"
-    t.index ["item_id"], name: "index_category_items_on_item_id"
-  end
-
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.decimal "price"
-    t.boolean "sold", default: false
     t.integer "quantity", default: 1
+    t.string "category"
     t.string "image"
     t.bigint "seller_id"
     t.datetime "created_at", precision: 6, null: false
@@ -63,8 +48,6 @@ ActiveRecord::Schema.define(version: 2021_12_30_190607) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "category_items", "categories"
-  add_foreign_key "category_items", "items"
   add_foreign_key "items", "users", column: "seller_id"
   add_foreign_key "ratings", "users", column: "buyer_id"
   add_foreign_key "ratings", "users", column: "seller_id"
